bc8062c0df46679536345fca519580feb2a723ad
alexey00921 <gdal_kamag@mail.ru>
2022-07-13 21:45:36 +0300

    twp commit

--- bage/bage.py
+++ bage/bage.py
@@ -10,3 +10,28 @@ def knapsack(W, weight, cost, n):
                 K[i][w] = K[i - 1][w]
     return K
 
+
+Y = [20, 5, 10, 40, 15, 25]
+X = [1, 2, 3, 8, 7, 4]
+W = 10;
+
+n = len(Y)
+
+print("Предметы:")
+for k in range(n):
+    print(k + 1, "\b) цена", Y[k], "вес", X[k])
+print("\nРюкзак вмещает вес:", W)
+
+K = knapsack(W, X, Y, n)
+
+print("\nЗаполнение рюкзака:")
+w, i, total_weight = W, n, 0
+res = K[n][W]
+while i > 0 and res > 0:
+    if res != K[i - 1][w]:
+        print(i, "\b-й предмет, ценой", Y[i - 1], "и веса", X[i - 1])
+        total_weight += X[i - 1]
+        res -= Y[i - 1]
+        w -= X[i - 1]
+    i -= 1
+print("\nОбщий вес предметов:", total_weight, "\b, стоимость:", K[n][W])
